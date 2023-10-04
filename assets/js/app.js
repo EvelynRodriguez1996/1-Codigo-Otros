73da09b721fb8b2187c1ed79808b49864b352405
Evelyn Casillas <sistem.out.println.hello.evelyn@gmail.com>
2023-10-03 23:54:48 -0600

    Se añaden comentarios y modificaciones en las lineas de código

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,28 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name');//añadi un punto que faltaba antes de la letra n de name por que es una clase
+const $b = document.querySelector('.blog');//sustitui "#" por "." porque es una clase
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+//Cambie de lugar la palabra async y la coloque antes de function ya que solo se puede usar await si es una funcion asincrona
+async function displayUser(username) {
+  
   $n.textContent = 'cargando...';
+  // try {
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
-}
+  const data = await response.json();  // Escribí  "await response.json()" para esperar la respuesta del servidor y guardar la informacion en la constante "data"
+  console.log(data); 
+
+  $n.textContent = data.name;
+  $b.textContent = data.blog; 
+  $l.textContent = data.location;
+} //La sintaxis fue modificada
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}` // Coloque "$" antes de la n.
 }
 
 displayUser('stolinski').catch(handleError);
+/* Este codigo se utiliza para llamar a la función "displayUser" pasando como argumento el nombre 'stolinski'. Luego, si se produce un error durante la ejecución de la función 'handleError' para manejar el error.*/
